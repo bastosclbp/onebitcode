93acfe4d372ae2581ea5d0f6633cf3814c898219
Carlos Leonardo <c.carlos-bp@hotmail.com>
2023-09-09 09:28:58 -0300

    while

--- JS/MODULO_1/estruturasRepeticao/while/while.js
+++ JS/MODULO_1/estruturasRepeticao/while/while.js
@@ -4,6 +4,10 @@ while (speed > 0) {
     alert("O carro está a " + speed + " Km/h")
     speed -= 20
     alert("Diminuindo 20 Km/h...")
+
+    if (speed === 40) {
+        break
+    }
 }
 
 alert("O carro parou.")
